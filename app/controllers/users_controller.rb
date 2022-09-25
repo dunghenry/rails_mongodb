f5c078ae9959e6baf0626eadc31febfb2e3bb4e4
dunghenry <trandungksnb6930082001>
2022-09-25 09:45:33 +0700

    crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,56 @@
 class UsersController < ApplicationController
-    def index
-        @users = User.all
+  before_action :getUser, only: [:showUser, :updateUser, :deleteUser]
+  protect_from_forgery with: :null_session
+  def getUsers
+    @users = User.all
+    if @users
+      render json: @users, status: :ok
+    else
+      render json: {msg: "Users is empty"}, status: :unprocessable_entity
     end
+  end
+  def showUser
+    if @user
+      render json: @user, status: :ok
+    else
+      render json:{msg: "User not found"}, status: :unprocessable_entity
+    end
+  end
+  def addUser
+    user = User.new(userparams)
+    if user.save()
+      render json: user, status: :ok
+    else
+      render json: {msg: "User not added", error: user.errors}, status: :unprocessable_entity
+    end
+  end
+  def updateUser
+    if @user
+      if @user.update(userparams)
+        render json: @user, status: :ok
+      else
+        render json:{msg: "Updated user failed", error: @user.errors},status: :unprocessable_entity
+      end
+    else
+      render json:{msg: "User not found"},status: :unprocessable_entity
+    end
+  end
+  def deleteUser
+    if @user
+      if @user.destroy()
+        render json:{msg: "Deleted user successfully"},status: :ok
+      else
+        render json:{msg: "Deleted user failed"},status: :unprocessable_entity
+      end
+    else
+      render json:{msg: "User not found"},status: :unprocessable_entity
+    end
+  end
+  private
+  def userparams
+    params.permit(:name, :email)
+  end
+  def getUser
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "users#index"
+  get 'users', action: :getUsers, controller: :users
+  post 'users', action: :addUser, controller: :users
+  get 'users/:id', action: :showUser, controller: :users
+  put "/users/:id", action: :updateUser, controller: :users
+  delete "/users/:id", action: :deleteUser, controller: :users
+  root "pages#index"
 end
